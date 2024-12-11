5e0c7d668ad17f3c3da40df1abd9119118a81c48
VladislavPetyukevich <pet.vladislav@gmail.com>
2024-12-11 20:11:57 +0300

    Fix handleUnauthorized

--- src/utils/handleUnauthorized.ts
+++ src/utils/handleUnauthorized.ts
@@ -1,10 +1,22 @@
+const redirectToLogin = () => {
+  window.location.href = 'please-pass-env-variable-for-login-page-url';
+};
+
 export const handleUnauthorized = async (response: Response) => {
   try {
     const responseJson = await response.json();
     if (responseJson.authentication) {
-      window.location.href = `oauth2/authorization/passport?redirect-location=${encodeURIComponent(window.location.href)}`;
+      const response = await fetch(`oauth2/authorization/passport?redirect-location=${encodeURIComponent(window.location.href)}`);
+      if (response.status !== 302) {
+        return redirectToLogin();
+      }
+      const locationHeader = response.headers.get('location');
+      if (!locationHeader) {
+        return redirectToLogin();
+      }
+      window.location.href = locationHeader;
       return;
     }
   } catch { };
-  window.location.href = 'please-pass-env-variable-for-login-page-url.com';
+  redirectToLogin();
 };
